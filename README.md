ef787d860aee1aac67916d72e15a26d2b771ce7e
Marcin Rutkowski <marcin.rutkowski@creative-it.pl>
2020-10-05 23:30:13 +0200

    chromedriver support

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # chromium-latest-linux
-Scripts to download and run the latest Linux build of Chromium.
+Scripts to download and run the latest Linux build of Chromium (with chromedriver).
+
+    ./update.sh - Run update (download & unpack latest revision)
+    ./run.sh - Run browser
+
+NOTE: chromedriver exec is copied to ./latest
--- update.sh
+++ update.sh
@@ -13,19 +13,34 @@ if [ -d $REVISION ] ; then
   exit
 fi
 
-ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
+fetch_url() {
+	pushd $REVISION
+	echo "fetching: $1"
+	curl -# "$1" > "$2"
+	echo "unzipping: $2"
+	unzip "$2"
+	[ -f "$2" ] && rm "$2"
+	popd
+}
 
+ZIP_URL="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchrome-linux.zip?alt=media"
 ZIP_FILE="${REVISION}-chrome-linux.zip"
+
+ZIP_URL_WD="https://www.googleapis.com/download/storage/v1/b/chromium-browser-snapshots/o/Linux_x64%2F$REVISION%2Fchromedriver_linux64.zip?alt=media"
+ZIP_FILE_WD="${REVISION}-chromedriver_linux64.zip"
 
 echo "fetching $ZIP_URL"
 
 rm -rf $REVISION
 mkdir $REVISION
-pushd $REVISION
-curl -# $ZIP_URL > $ZIP_FILE
-echo "unzipping.."
-unzip $ZIP_FILE
-popd
+
+fetch_url $ZIP_URL $ZIP_FILE
+fetch_url $ZIP_URL_WD $ZIP_FILE_WD
+
 rm -f ./latest
 ln -s $REVISION/chrome-linux/ ./latest
 
+if [ -f $REVISION/chromedriver_linux64/chromedriver ]; then
+	mv $REVISION/chromedriver_linux64/chromedriver ./latest/
+	rmdir $REVISION/chromedriver_linux64
+fi;
